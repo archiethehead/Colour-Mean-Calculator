1d477b8a6a03367b0d94be340cb8998a18ea358c
Luke Cheshire <lcheshire3335@icloud.com>
2026-02-01 03:47:31 +0000

    feat: added the basic foundations of the GUI using tkinter

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import tkinter as tk
+from tkinter import colorchooser
+
 from hex_calculator.calculator import hex_mean
+
+
 
 test = ["004b9d",
         "202f55",
@@ -8,4 +13,23 @@ test = ["004b9d",
         "0d2b60",
         "123576"]
 
+root = tk.Tk()
+root.title("Colour Mean Calculator")
+root.geometry("600x400")
+
+label = tk.Label(root, text = test)
+label.pack()
+
+label2 = tk.Label(root, text = hex_mean)
+label2.pack()
+
+label3 = tk.Label(root, text = "The mean colour is: " + hex_mean(test))
+label3.pack()
+
+
+frame = tk.Frame(root)
+frame.pack()
+
 print(hex_mean(test))
+
+root.mainloop()
