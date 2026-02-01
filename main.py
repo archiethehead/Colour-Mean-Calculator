c227554f9adfd1fc587ed7be81dadf915426703f
Archie Healy <archiethehead@gmail.com>
2026-02-01 02:25:51 +0000

    Main
    Chore: Created the main.py file, ready to be populated with the GUI

---
+++ main.py
@@ -0,0 +1,11 @@
+from hex_calculator.calculator import hex_mean
+
+test = ["004b9d",
+        "202f55",
+        "13c2f0",
+        "003882",
+        "2472ce",
+        "0d2b60",
+        "123576"]
+
+print(hex_mean(test))
